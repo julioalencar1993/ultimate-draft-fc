5e8df880afb45260e66a5e1e614ce5a96ca8036e
julioalencar1993 <julio.alencar2021@gmail.com>
2026-06-16 16:18:46 -0400

    alteracao

--- server.js
+++ server.js
@@ -1,83 +1,24 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-
 const io = new Server(server);
 
-app.use(express.static("."));
+app.use(express.static(__dirname));
 
-const rooms = {};
-
-function generateCode() {
-  return Math.floor(
-    10000000 + Math.random() * 90000000
-  ).toString();
-}
-
-io.on("connection", (socket) => {
-
-  console.log("Jogador conectado");
-
-  socket.on("createRoom", ({ password, club }) => {
-
-    const code = generateCode();
-
-    rooms[code] = {
-      password,
-      players: [{
-        id: socket.id,
-        club
-      }]
-    };
-
-    socket.join(code);
-
-    socket.emit("roomCreated", {
-      code
-    });
-
-  });
-
-  socket.on("joinRoom", ({ code, password, club }) => {
-
-    const room = rooms[code];
-
-    if (!room) {
-      socket.emit(
-        "errorJoin",
-        "Sala não encontrada"
-      );
-      return;
-    }
-
-    if (room.password !== password) {
-      socket.emit(
-        "errorJoin",
-        "Senha incorreta"
-      );
-      return;
-    }
-
-    room.players.push({
-      id: socket.id,
-      club
-    });
-
-    socket.join(code);
-
-    io.to(code).emit(
-      "playerList",
-      room.players
-    );
-
-  });
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-server.listen(
-  process.env.PORT || 3000,
-  () => console.log("Servidor iniciado")
-);
+io.on("connection", (socket) => {
+  console.log("Jogador conectado:", socket.id);
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Servidor iniciado na porta " + PORT);
+});
